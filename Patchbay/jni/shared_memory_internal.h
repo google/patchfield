4485478a871d3fafc688caa85aa0189af7757dc9
Peter Brinkmann <peter.brinkmann@gmail.com>
2013-07-12 20:12:48 -0400

    .

--- Patchbay/jni/shared_memory_internal.h
+++ Patchbay/jni/shared_memory_internal.h
@@ -1,3 +1,8 @@
+/*
+ * Tools for handling shared memory with ashmem. Most functions simply wrap basic
+ * system calls, except for smi_{send,receive}, which serve to pass the ashmem
+ * file descriptor across process boundaries using Unix domain sockets.
+ */
 #ifndef __SHARED_MEMORY_INTERNAL_H__
 #define __SHARED_MEMORY_INTERNAL_H__
 
