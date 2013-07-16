d8c3369a0c8ccdb2d99ea43086ccab642758c9d3
Peter Brinkmann <peter.brinkmann@gmail.com>
2013-07-15 20:47:21 -0400

    .

--- Patchbay/jni/utils/buffer_size_adapter.h
+++ Patchbay/jni/utils/buffer_size_adapter.h
@@ -1,18 +1,34 @@
+/*
+ * An adapter that allows audio modules to operate at buffer sizes that differ
+ * from the buffer size of the Patchbay service.
+ */
 #ifndef __BUFFER_SIZE_ADAPTER_H__
 #define __BUFFER_SIZE_ADAPTER_H__
 
 #include "audio_module.h"
 
+/*
+ * Abstract data type representing a buffer size adapter.
+ */
 typedef struct _buffer_size_adapter buffer_size_adapter;
 
+/*
+ * Similar to am_create (see audio_module.h).
+ */
 buffer_size_adapter *bsa_create(
     int version, int token, int index,
     int host_buffer_frames, int user_buffer_frames,
     int input_channels, int output_channels,
     audio_module_process_t user_process, void *user_context);
 
+/*
+ * Similar to am_release (see audio_module.h).
+ */
 void bsa_release(buffer_size_adapter *adapter);
 
+/*
+ * Returns the audio module runner instance that's backing the given adapter.
+ */
 audio_module_runner *bsa_get_runner(buffer_size_adapter *adapter);
 
 #endif
