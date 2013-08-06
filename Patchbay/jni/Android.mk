6fe947e77b1291cbb8e8aebd944e01e7335648fe
Peter Brinkmann <peter.brinkmann@gmail.com>
2013-08-06 13:07:19 -0400

    Further cleanup.

--- Patchbay/jni/Android.mk
+++ Patchbay/jni/Android.mk
@@ -5,7 +5,8 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := audiomodule
-LOCAL_STATIC_LIBRARIES := audiomoduleinternal
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
+LOCAL_EXPORT_CFLAGS := -Wno-int-to-pointer-cast -Wno-pointer-to-int-cast
 LOCAL_SRC_FILES := audio_module.c
 include $(BUILD_STATIC_LIBRARY)
 
@@ -40,7 +41,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := javamodule
 LOCAL_LDLIBS := -llog
-LOCAL_SRC_FILES := modules/javamodule.c
+LOCAL_SRC_FILES := modules/javamodule.c internal/simple_barrier.c
 LOCAL_STATIC_LIBRARIES := audiomodule buffersizeadapter
 include $(BUILD_SHARED_LIBRARY)
 
@@ -68,7 +69,6 @@ include $(BUILD_SHARED_LIBRARY)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := patchbay
-LOCAL_CFLAGS := -Wno-int-to-pointer-cast -Wno-pointer-to-int-cast
 LOCAL_STATIC_LIBRARIES := audiomoduleinternal
 LOCAL_SRC_FILES := internal/patchbay.c
 include $(BUILD_SHARED_LIBRARY)
